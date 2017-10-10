ae6c983a8f45e6d25d1c0033d1d0f1ce832515a4
Vitalik Lavrenko <vitaliklavrenko@MacBook-Pro-Vitalik.local>
2017-10-10 13:14:47 +0300

    Update Homework 1

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-var arrayNumbers = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,undefined,NaN];
+var arrayNumbers = [3,0,-5,1,'qwerty','2ad',5342,-653,'ewfr',true,78,42,-12,2,-3,4,undefined,NaN];
 var min;
 var max;
 var sum = null;
-var num;
 var i;
 
-	for (i = 0; i < arrayNumbers.length; i++){
-	     if(!isNaN(arrayNumbers[i])) {
+	for (i = 0; i < arrayNumbers.length; i++) {
+	  if( !isNaN(arrayNumbers[i]) ) {
         sum += arrayNumbers[i]
-    	     }
-	}
-	console.log('SUM = ' + sum);
+      }
+    }
+    console.log('SUM = ' + sum);
 
-	i = arrayNumbers.length, min = arrayNumbers[0], num = arrayNumbers
-	while (i--){
-		if (arrayNumbers[i] < min) {
-			min = arrayNumbers[i];
-			num = i;
-		}
+	i = arrayNumbers.length;
+	min = arrayNumbers[0];
+	while (min < i) {
+	  if (arrayNumbers[i] < min ) {
+	    min = arrayNumbers[i]
+	  }
+	  i--;
 	}
 	console.log('MIN = ' + min);
 
 	for (i = 0; i < arrayNumbers.length; i++) { 
-    		if(!isNaN(arrayNumbers[i])) {
-            		if(arrayNumbers[i] > max) {
-                		max = arrayNumbers[i]
-            		}
-			else if (i == 0) {
- 				max = arrayNumbers[i]
- 			}
- 		}
+      if( arrayNumbers[i] > max || typeof max === 'undefined') {
+        max = arrayNumbers[i]
+ 	  }
  	}
-    	console.log('MAX = ' + max)
+    console.log('MAX = ' + max)
 
